736d4bdf9170156606441c5f93eeee63dc9ea725
GabrielAfflitto <gafflitto92@gmail.com>
2022-08-23 22:09:48 -0600

    added GET route for users

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,8 @@
+const index = require('../controllers/usersController').index;
+
 const express = require('express')
 
 const router = express.Router();
+router.get('/users', index)
 
 module.exports = router;
